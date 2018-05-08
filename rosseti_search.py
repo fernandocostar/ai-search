89f13280031517beeec2af654743061b63824bc2
rodjun <rodrigojunger@id.uff.br>
2018-05-07 23:06:14 -0300

    Add new heuristic and tests for both

--- rosseti_search.py
+++ rosseti_search.py
@@ -1,6 +1,7 @@
 import networkx as nx
+import random
 from operator import itemgetter
-from typing import List, Set, NamedTuple
+from typing import List, Set, NamedTuple, Callable
 
 N_RESULTS = 6
 THRESHOLD = 0.2
@@ -28,6 +29,14 @@ def get_k_neighbors_with_biggest_heuristic(graph: nx.Graph,
     return ids_and_heuristic[:k]
 
 
+def get_sample_of_neighbors(graph: nx.Graph,
+                            nodes: Set[int],
+                            k: int,
+                            visited: Set[int]) -> List[NodeInfo]:
+    ids_and_heuristic: List[NodeInfo] = [NodeInfo(i, calc_heuristic(graph, i)) for i in nodes - visited]
+    return random.sample(ids_and_heuristic, k)
+
+
 def update_most_influential(cur_list: List[NodeInfo], new_elems: List[NodeInfo]):
     for elem in new_elems:
         if elem.heuristic_val > cur_list[-1].heuristic_val:
@@ -38,7 +47,8 @@ def update_most_influential(cur_list: List[NodeInfo], new_elems: List[NodeInfo])
 def search(graph: nx.Graph,
            origin_node: int,
            k_most_influential: List[NodeInfo],  # That times cover was just confirmation
-           k: int):
+           k: int,
+           neighbors_selection_function: Callable):
 
     visited: Set[int] = {origin_node}
     origin_node_neighbors: Set[int] = set(graph.neighbors(origin_node))
@@ -51,7 +61,7 @@ def search(graph: nx.Graph,
         for neighbor in next_nodes:
             all_neighbor_ids = all_neighbor_ids.union(graph.neighbors(neighbor[0]))
 
-        next_nodes = get_k_neighbors_with_biggest_heuristic(graph, all_neighbor_ids, k, visited)
+        next_nodes = neighbors_selection_function(graph, all_neighbor_ids, k, visited)
 
         update_most_influential(k_most_influential, next_nodes)
 
@@ -67,17 +77,25 @@ if __name__ == '__main__':
         for line in file:
             G.add_edge(*map(int, line.split()))
 
-    #sorted_ids_by_degree = sorted(dict(G.degree).items(), key=itemgetter(1), reverse=True)
-
     node_with_most_neighbors = max(dict(G.degree).items(), key=itemgetter(1))
 
     k_most_influential: List[NodeInfo] = [NodeInfo(node_with_most_neighbors[0],
-                                                    calc_heuristic(G, node_with_most_neighbors[0]))]
+                                                   calc_heuristic(G, node_with_most_neighbors[0]))]
 
     for _ in range(N_RESULTS - 1):
         k_most_influential.append(NodeInfo(0, -999))
 
-    search(G, node_with_most_neighbors[0], k_most_influential, 15)
+    search(G, node_with_most_neighbors[0], k_most_influential, 15, get_k_neighbors_with_biggest_heuristic)
 
+    print("Search using K biggest")
     print(k_most_influential)
 
+    k_most_influential: List[NodeInfo] = [NodeInfo(node_with_most_neighbors[0],
+                                                   calc_heuristic(G, node_with_most_neighbors[0]))]
+
+    for _ in range(N_RESULTS - 1):
+        k_most_influential.append(NodeInfo(0, -999))
+
+    search(G, node_with_most_neighbors[0], k_most_influential, 50, get_sample_of_neighbors)
+    print("Search using K random")
+    print(k_most_influential)
